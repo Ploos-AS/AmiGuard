5aec5e70df57446514075e25892368c72be58cc4
pos <pgousdal@users.noreply.github.com>
2026-09-15 07:13:16 +0200

    m3.2: define bounded raw floppy read API

--- src/trackdisk.h
+++ src/trackdisk.h
@@ -5,11 +5,22 @@
 
 #define AMIGUARD_MAX_FLOPPY_UNIT 3
 #define AMIGUARD_BOOTBLOCK_BYTES 1024UL
+#define AMIGUARD_DD_FLOPPY_BYTES 901120UL
+#define AMIGUARD_TRACKDISK_SECTOR_BYTES 512UL
 
 LONG amiguard_read_bootblock(
     UBYTE unit,
     unsigned char *buffer,
     ULONG buffer_size
+);
+
+/* Read one bounded region without ever issuing a write command. */
+LONG amiguard_read_raw_region(
+    UBYTE unit,
+    ULONG offset,
+    unsigned char *buffer,
+    ULONG length,
+    ULONG media_limit
 );
 
 #endif
